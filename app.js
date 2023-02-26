8d485ecfc0b016ffae0018d42c1974324e57b663
Arka <arkac720@gmail.com>
2023-02-26 11:16:01 +0530

    assignment 6

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-// const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-
 const shopRoutes = require('./routes/shop');
-
-const bodyParser = require('body-parser');
+const contactRoutes = require('./routes/contact')
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
-
-app.use('/',shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found!!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-// const server = http.createServer(app);
-
-// server.listen(4000);
-
-app.listen(4000)
+app.listen(2000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  console.log("in other middleware");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button>Add Product</button><form action="/product" method="POST"><input type="text" name="size" /><button>Add Size Of Product</button></form></form>'
-  );
-  // res.send('')
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post("/add-product", (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
